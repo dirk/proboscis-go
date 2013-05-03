d1952f8eeeeb206298f44155e3370424c0bb19be
Dirk Gadsden <dirk@esherido.com>
2013-05-03 16:55:52 -0400

    Start adding tests

---
+++ proboscis_test.go
@@ -0,0 +1,15 @@
+package proboscis
+
+import (
+  "testing"
+  "fmt"
+)
+
+func TestNewRequest(t *testing.T) {
+  req := NewRequest()
+  if len(req.Data) != 0 {
+    t.Fatal("Request should have empty data; currently:", len(req.Data))
+  } else { fmt.Println("PASS TestNewRequest/len(req.Data)") }
+  
+  
+}
