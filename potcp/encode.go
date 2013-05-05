547e457ecff8bd5ce40e6b009b92a3cf12a1e534
Dirk Gadsden <dirk@esherido.com>
2013-05-04 23:38:48 -0400

    Bugfix in coder

--- potcp/encode.go
+++ potcp/encode.go
@@ -23,7 +23,10 @@ func EncodeRequest(req *proboscis.Request, w io.Writer) error {
   w.Write(req.Data)
   return nil
 }
-func DecodeRequest(r bufio.Reader) (*proboscis.Request, error) {
+func DecodeRequest(reader io.Reader) (*proboscis.Request, error) {
+  var r *bufio.Reader
+  r = bufio.NewReader(reader)
+  
   var req *proboscis.Request
   var method, format, length_string string
   var length, read int
@@ -31,13 +34,13 @@ func DecodeRequest(r bufio.Reader) (*proboscis.Request, error) {
   
   // TODO: At least some sanity-checking
   method, _ = r.ReadString(period_byte)
-  method = method[0:len(method) - 2]
+  method = method[0:len(method) - 1]
   
   format, _ = r.ReadString(colon_byte)
-  format = format[0:len(format) - 2]
+  format = format[0:len(format) - 1]
   
   length_string, _ = r.ReadString(colon_byte)
-  length_string = length_string[0:len(length_string) - 2]
+  length_string = length_string[0:len(length_string) - 1]
   
   length, _ = strconv.Atoi(length_string)
   
@@ -53,7 +56,7 @@ func DecodeRequest(r bufio.Reader) (*proboscis.Request, error) {
   req = proboscis.NewRequest()
   req.Method = method
   req.Format = format
-  req.Length = uint32(length)
+  req.Length = length
   req.Data   = data
   return req, nil
 }
